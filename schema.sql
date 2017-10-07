e496eca2b71eac65403ee09f53f8ae25efb1d4bd
Rene Lee <renelee168@gmail.com>
2017-10-07 11:07:43 -0700

    updated app.py

--- schema.sql
+++ schema.sql
@@ -4,12 +4,14 @@ create table users (
   username text not null,
   password text not null,
   venmo text not null,
-  phone_number integer not null
+  phone_number integer not null,
+  rating integer 5,
+  number_of_ratings integer 0,
 );
 
 drop table if exists listings;
 create table listings (
 	user_id integer not null;
-	
+
 
 );
